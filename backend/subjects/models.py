08241d18eb1b1ce1aeb23b73a7e26cd31cc96244
bhavika232 <bhavika06khanna@gmail.com>
2026-06-12 00:52:32 +0530

    added subject model

--- backend/subjects/models.py
+++ backend/subjects/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
+class Subject(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="subjects"
+    )
+
+    subject_name = models.CharField(max_length=100)
+    professor_name = models.CharField(max_length=100)
+
+    credits = models.IntegerField(default=3)
+    threshold = models.IntegerField(default=75)
+    classes_per_week = models.IntegerField(default=3)
+
+    def __str__(self):
+        return self.subject_name
